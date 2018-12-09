231d64243bb643ca12aa588ed0c74c47390c1586
GreenWyvern <gamevingw@gmail.com>
2018-12-09 16:30:05 -0500

    Basic Player/Enemy Controls/Behavior

---
+++ This Project Is Hopeless/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    NavMeshAgent navAgent;
+    NavMeshObstacle navOb;
+
+    private bool alive = true;
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        navOb = GetComponent<NavMeshObstacle>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alive)
+        {
+            navAgent.destination = player.transform.position;
+        }
+        else
+        {
+
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "sword")
+        {
+            navAgent.enabled = false;
+            navOb.enabled = true;
+            Debug.Log("oof");
+            alive = false;
+        }
+    }
+}
---
+++ This Project Is Hopeless/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float movespeed;
+    public float rotatespeed;
+	
+
+    void Update()
+    {
+
+        var x2 = Input.GetAxis("Horizontal2") * Time.deltaTime * 3.0f;
+        var z2 = Input.GetAxis("Vertical2") * Time.deltaTime * 3.0f;
+
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical1") * movespeed);
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal1") * movespeed);
+        
+        var x = Input.GetAxis("Horizontal2") * Time.deltaTime * rotatespeed;
+        transform.Rotate(0, x, 0);
+
+    }
+}
